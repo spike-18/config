9e21ecf0a1e21dac72c6cb8b4792b936c2b8a65b
Kirill Madorskii <madorskii.kv@phystech.edu>
2025-08-09 02:21:13 +0300

    lsp done

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,75 +1,109 @@
+-- Neovim LSP setup with lazy.nvim
 return {
-
-  -- lsp servers
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = { virtual_text = { prefix = "icons" } },
-      capabilities = {
-        workspace = {
-          didChangeWatchedFiles = {
-            dynamicRegistration = false,
-          },
+    "neovim/nvim-lspconfig", -- Core LSP client configs
+    dependencies = {
+      { "williamboman/mason.nvim", config = true }, -- LSP/DAP/Linter installer
+      "williamboman/mason-lspconfig.nvim", -- Bridge Mason ↔ lspconfig
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      -- Auto-install these servers
+      mason_lspconfig.setup({
+        ensure_installed = {
+          "lua_ls", -- Lua
+          "pyright", -- Python
+          "clangd", -- C/C++
+          "rust_analyzer", -- Rust
         },
-      },
-      servers = {
+      })
+
+      -- Default attach function (keymaps etc.)
+      local on_attach = function(_, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
+
+      -- LSP settings per server
+      local servers = {
         lua_ls = {
-          -- cmd = { "/home/folke/projects/lua-language-server/bin/lua-language-server" },
-          -- single_file_support = true,
           settings = {
             Lua = {
-              misc = {
-                -- parameters = { "--loglevel=trace" },
-              },
-              hover = { expandAlias = false },
-              type = {
-                castNumberToInteger = true,
-                inferParamType = true,
-              },
-              diagnostics = {
-                disable = { "incomplete-signature-doc", "trailing-space" },
-                -- enable = false,
-                groupSeverity = {
-                  strong = "Warning",
-                  strict = "Warning",
-                },
-                groupFileStatus = {
-                  ["ambiguity"] = "Opened",
-                  ["await"] = "Opened",
-                  ["codestyle"] = "None",
-                  ["duplicate"] = "Opened",
-                  ["global"] = "Opened",
-                  ["luadoc"] = "Opened",
-                  ["redefined"] = "Opened",
-                  ["strict"] = "Opened",
-                  ["strong"] = "Opened",
-                  ["type-check"] = "Opened",
-                  ["unbalanced"] = "Opened",
-                  ["unused"] = "Opened",
-                },
-                unusedLocalExclude = { "_*" },
-              },
+              diagnostics = { globals = { "vim" } },
+              workspace = { checkThirdParty = false },
             },
           },
         },
-      },
-    },
+        pyright = {},
+        clangd = {},
+        rust_analyzer = {},
+      }
+
+      -- Setup handlers for each server
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            on_attach = on_attach,
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            settings = servers[server_name],
+          })
+        end,
+      })
+    end,
   },
 
   -- Lightweight yet powerful formatter plugin for Neovim
   {
     "stevearc/conform.nvim",
-    optional = true,
+
     opts = {
-      linters_by_ft = {
-        lua = { "selene", "luacheck" },
+      formatters_by_ft = {
+        lua = { "stylua" },
         python = { "ruff_organize_imports", "ruff_format" },
         rust = { "rustfmt" },
-	c = { "clang_format" },
-	cpp = { "clang_format" }
+        c = { "clang_format" },
+        cpp = { "clang_format" },
       },
       formatters = {},
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
     },
   },
 
+  -- Linter
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        lua = { "selene", "luacheck" },
+      },
+      linters = {
+        selene = {
+          condition = function(ctx)
+            local root = LazyVim.root.get({ normalize = true })
+            if root ~= vim.uv.cwd() then
+              return false
+            end
+            return vim.fs.find({ "selene.toml" }, { path = root, upward = true })[1]
+          end,
+        },
+        luacheck = {
+          condition = function(ctx)
+            local root = LazyVim.root.get({ normalize = true })
+            if root ~= vim.uv.cwd() then
+              return false
+            end
+            return vim.fs.find({ ".luacheckrc" }, { path = root, upward = true })[1]
+          end,
+        },
+      },
+    },
+  },
 }
